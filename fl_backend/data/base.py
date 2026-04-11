c52d4defc86f787925747707b48f694e77a019db
XxRedPanda <philippway@gmail.com>
2026-04-11 18:21:55 +0200

    small changes and scripts from ailab

--- fl_backend/data/base.py
+++ fl_backend/data/base.py
@@ -1,14 +1,12 @@
 from abc import ABC, abstractmethod
 
-#this is supposed to be an interface for all the data handlers
-# so each Data handler has these methods implemented, 
-# and the rest of the code can call these methods without worrying about the implementation details of each data handler
+
 class BaseDatasetHandler(ABC):
-    def __init__(self, config:dict):
+    def __init__(self, config: dict):
         self.config = config
 
     @abstractmethod
-    def get_dataloaders(self, partion_id:int):
+    def get_dataloaders(self, partition_id: int):
         pass
 
     @abstractmethod
