f36a93e46b7e3ad54e6c1cf213273e2e5f911cc9
Sooonnnaaa <Bubulspam@gmail.com>
2026-04-12 15:21:23 +0200

    fikset imports i plot config

--- fl_backend/plotting/plotting_config.py
+++ fl_backend/plotting/plotting_config.py
@@ -1,6 +1,6 @@
 from plotting.plot_methods.confusion_matrix_plot import confusion_matrix_plot
-from plot_methods.precision_recall_plot import precision_recall_plot
-from plot_methods.loss_accuracy_plot import loss_over_time_plot
+from plotting.plot_methods.precision_recall_plot import precision_recall_plot
+from plotting.plot_methods.loss_accuracy_plot import loss_over_time_plot
 
 def plot_diagrams(targets, preds, train_losses, train_accuracies):
     #Udvid denne når den nye config struktur er blevet implementeret så det hele passer
