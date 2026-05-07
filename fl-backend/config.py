0038b16eef4bbf702af0795a40fc38e0b482a02d
Jasper Seneberg <bananfi2k@gmail.com>
2026-05-07 22:02:38 +0200

    make sure context_lenght is alway the same as window_size

--- fl-backend/config.py
+++ fl-backend/config.py
@@ -123,7 +123,7 @@ class PatchTSTConfig():
 
     name:                str   = "patchtst"
     batch_size:          int   = 64
-    context_length:      int   = 168
+    context_length:      int   = 168 # Has to mach the window_size used in data config
     patch_length:        int   = 16
     patch_stride:        int   = 8
     d_model:             int   = 128
@@ -229,9 +229,9 @@ class LeadCSVConfig:
     gap_hours:    int   = 73
     stride:       int   = 168
 
-    def build_handler(self, config=None):
+    def build_handler(self, config):
         from data.lead_csv import LeadCSVHandler
-        return LeadCSVHandler(config or CONFIG)
+        return LeadCSVHandler(config)
 
 @dataclass
 class PowerGridCSVConfig:
@@ -308,6 +308,10 @@ class ExperimentConfig:
     federation: FederationConfig             = field(default_factory=FederationConfig)
     evaluation: EvaluationConfig             = field(default_factory=EvaluationConfig)
     interpretability: InterpretabilityConfig = field(default_factory=InterpretabilityConfig)
+
+    def __post_init__(self):
+        if hasattr(self.model, "context_length"):
+            self.model.context_length = self.data.window_size
 
 
 CONFIG = ExperimentConfig()
